cc5a83b068814b0e691954e7920c1933b213dc2a
Dawid <d.lewkowicz.498@studms.ug.edu.pl>
2025-10-14 15:26:31 +0200

    feature 3

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -10,3 +10,8 @@ class TestAccount:
         account.balance += 100.05
         assert account.balance == 100.05
         assert account.pesel == "12345678901"
+        if (len(account.pesel) == 11):
+            assert account.pesel == "12345678901"
+        else:
+            assert account.pesel == "Invalid"
+        
