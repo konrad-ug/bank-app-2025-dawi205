f7d824c1b418ff8bc16b5de3e998d69ae0f78296
Dawid <d.lewkowicz.498@studms.ug.edu.pl>
2025-10-14 15:31:59 +0200

    feature 3

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,13 +3,12 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", "12345678901")
+        account = Account("John", "Doe", "12345678901111")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
         account.balance += 100.05
         assert account.balance == 100.05
-        assert account.pesel == "12345678901"
         if (len(account.pesel) == 11):
             assert account.pesel == "12345678901"
         else:
