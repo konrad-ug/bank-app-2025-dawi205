0ab542ed8b0efabad224890bcc6314b32f17449d
Dawid <d.lewkowicz.498@studms.ug.edu.pl>
2025-11-20 16:57:59 +0100

    refactor #2

--- tests/unit/test_account.py
+++ tests/unit/test_account.py
@@ -1,37 +1,53 @@
+import pytest
 from src.account import Account
 
+@pytest.fixture
+def create_account():
+    return Account
+
+@pytest.fixture
+def account_with_500(create_account):
+    account = create_account()
+    account.balance = 500
+    account.express_fee = 5
+    return account
+
 class TestAccount:
-    def test_history(self):
-        account = Account()
-        account.balance = 500.0
-        account.express_fee = 5.0
-        account.outcoming_express_transfer(300)
-        assert account.history == [-300, -5.0]
 
-    
-    def test_history_instant_not_enough_money(self):
-        account = Account()
-        account.balance = 500.0
-        account.express_fee = 5.0
-        account.outcoming_express_transfer(3000)
-        assert account.history == []
-    
-    
-    def test_history_outcoming_and_incoming(self):
-        account = Account()
-        account.balance = 500.0
-        account.express_fee = 5.0
-        account.outcoming_express_transfer(300)
-        account.incoming_transfer(1000)
-        account.outcoming_transfer(100)
-        assert account.history == [-300.0, -5.0, 1000.0, -100]
-
-    
-    def test_history_not_enough_money(self):
-        account = Account()
-        account.balance = 500.0
-        account.express_fee = 1.0
-        account.outcoming_transfer(300.0)
-        account.incoming_transfer(1000)
-        account.outcoming_express_transfer(45000)
-        assert account.history == [-300, 1000]
+    @pytest.mark.parametrize(
+            "amount, expected_history, expected_balance, expected_info1, expected_info2",
+            [
+                (50, [-50, 100, -50], 500, True, True),
+                (1000, [2000, -1000], 1500, False, True),
+                (500, [-500, 1000, -500], 500, True, True),
+                (550, [1100, -550], 1050, False, True),
+            ]
+    )
+    def test_history(self,account_with_500, amount, expected_history, expected_info1, expected_info2, expected_balance):
+        account = account_with_500
+        info1 = account.outcoming_transfer(amount)
+        account.incoming_transfer(amount*2)
+        info2 = account.outcoming_transfer(amount)
+        assert info1 == expected_info1
+        assert account.history == expected_history
+        assert info2 == expected_info2
+        assert account.balance == expected_balance
+        
+    @pytest.mark.parametrize(
+                "amount, expected_history, expected_balance, expected_info1, expected_info2",
+                [
+                    (50, [-50, -5, 100, -50, -5], 490, True, True),
+                    (1000, [2000, -1000, -5], 1495, False, True),
+                    (500, [-500, -5, 1000, -500, -5], 490, True, True),
+                    (550, [1100, -550, -5], 1045, False, True),
+                ]
+    )
+    def test_history_with_express_transfers(self, account_with_500, amount, expected_history, expected_balance, expected_info1, expected_info2):
+        account = account_with_500
+        info1 = account.outcoming_express_transfer(amount)
+        account.incoming_transfer(amount*2)
+        info2 = account.outcoming_express_transfer(amount)
+        assert info1 == expected_info1
+        assert account.history == expected_history
+        assert info2 == expected_info2
+        assert account.balance == expected_balance        
